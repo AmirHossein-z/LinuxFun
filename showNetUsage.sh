deae46de6067f631d83b2dc5a07e761b092d7788
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2022-03-17 07:49:39 +0330

    showNetUsage hourly  script file added

--- showNetUsage.sh
+++ showNetUsage.sh
@@ -3,5 +3,5 @@
 # for installing vnstat use this link -> https://www.cyberciti.biz/faq/ubuntu-install-vnstat-console-network-traffic-monitor/
 
 # for Installing notify-send use this link -> https://command-not-found.com/notify-send
-message=$(vnstat -h | cut -f3 -d "|" | grep "B")
+message=$(vnstat -h | cut -f3 -d "|" | grep "B" | tail -1)
 notify-send -t 4000 -i "dialog-information" "Your net usage in last hour is: $message"
